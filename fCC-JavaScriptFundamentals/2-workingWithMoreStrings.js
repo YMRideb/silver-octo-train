cca4b40b5674417fa0ab51f2aeee4b7ad20ab643
Yukio Rideb <103914884+YMRideb@users.noreply.github.com>
2026-02-09 06:14:55 +0000

    still working with strings

---
+++ fCC-JavaScriptFundamentals/2-workingWithMoreStrings.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName ="Neo";
+let botLocation = "Planet Earth";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName +" and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is very flexible and has many different uses.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " " + "was created in just 10 days!";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "'s" + " " + "original name was Mocha!";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation +".");
