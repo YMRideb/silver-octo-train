d2b69df70bf3a1241bf70198fb84e903e02ec8eb
Yukio Rideb <103914884+YMRideb@users.noreply.github.com>
2026-02-09 18:37:25 +0000

    still working with strings, save to close connection to workspace

---
+++ fCC-JavaScriptFundamentals/3-workingWithStrings.js
@@ -0,0 +1,25 @@
+let adjective = "shiny";
+let noun = "astronaut";
+let verb = "phasing";
+let place = "spaceship";
+let adjective2 = "broken";
+let noun2 = "meteroids";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory);
+
+let secondStory;
+
+adjective = "fuzzy";
+noun = "otter";
+verb = "swimming";
+place = "dock";
+adjective2 = "blue";
+noun2 = "boats";
+
+secondStory =  "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
